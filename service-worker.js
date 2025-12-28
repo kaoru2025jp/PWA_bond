2719444a6bacf4948183cfe7cd5b7abf27b975a8
kaoru2025jp <kaorusaku1@gmail.com>
2025-12-28 11:09:16 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+{
+  "name": "STOCKOFF23",
+  "short_name": "STOCKOFF23",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#4a90e2",
+  "icons": [
+    {
+      "src": "https://upload.wikimedia.org/wikipedia/commons/3/3f/Icon-round-Question_mark.jpg",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
